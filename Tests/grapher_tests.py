f6bfa4fbc75b18c40e8efae0d1f86248ceb782e1
duganc <c.m.s.dugan@gmail.com>
2017-02-01 11:10:44 -0500

    Updated test / image names to reference post.

--- Tests/grapher_tests.py
+++ Tests/grapher_tests.py
@@ -65,14 +65,14 @@ class ComplexGrapherTests(unittest.TestCase):
         png = cg.save_as_png("test_plot_arc_arrowed_points")
         verify(str(hash(png)), self.reporter)
 
-    def test_figure_1(self):
+    def test_trig_post_figure_1(self):
         cg = grapher.ComplexGrapher()
         cg.add_point(complex(2, 1), label=True)
         cg.add_point(complex(-3, 2), label=True)
-        png = cg.save_as_png("figure_1_two_labeled_points")
+        png = cg.save_as_png("trig_post_figure_1_two_labeled_points")
         verify(str(hash(png)), self.reporter)
 
-    def test_figure_2(self):
+    def test_trig_post_figure_2(self):
         cg = grapher.ComplexGrapher()
         z0 = complex(2, 1)
         z1 = complex(-3, 2)
@@ -81,49 +81,49 @@ class ComplexGrapherTests(unittest.TestCase):
         cg.add_point(z0 + z1, label=True, color='r')
         cg.add_arrow(complex(0, 0), z1)
         cg.add_arrow(z0, z0 + z1)
-        png = cg.save_as_png("figure_2_two_points_and_their_sum")
+        png = cg.save_as_png("trig_post_figure_2_two_points_and_their_sum")
         verify(str(hash(png)), self.reporter)
 
-    def test_figure_3(self):
+    def test_trig_post_figure_3(self):
         cg = grapher.ComplexGrapher()
         z0 = complex(2, 1)
         z1 = complex(-3, 2)
         cg.add_point(z0, label=True)
         cg.add_point(z1, label=True)
         cg.add_point(z0*z1, label=True, color='r')
-        png = cg.save_as_png("figure_3_two_points_and_their_product")
+        png = cg.save_as_png("trig_post_figure_3_two_points_and_their_product")
         verify(str(hash(png)), self.reporter)
 
-    def test_figure_4(self):
+    def test_trig_post_figure_4(self):
         cg = grapher.ComplexGrapher()
         z0 = complex(0, 2)
         z1 = complex(-3, 2)
         cg.add_point(z0, label=True)
         cg.add_point(z1, label=True)
         cg.add_point(z0*z1, label=True, color='r')
-        png = cg.save_as_png("figure_4_multiplication_by_pure_imaginary")
+        png = cg.save_as_png("trig_post_figure_4_multiplication_by_pure_imaginary")
         verify(str(hash(png)), self.reporter)
 
-    def test_figure_5(self):
+    def test_trig_post_figure_5(self):
         cg = grapher.ComplexGrapher()
         z0 = complex(0, -2)
         z1 = complex(-3, 2)
         cg.add_point(z0, label=True)
         cg.add_point(z1, label=True)
         cg.add_point(z0*z1, label=True, color='r')
-        png = cg.save_as_png("figure_5_multiplication_by_pure_negative_imaginary")
+        png = cg.save_as_png("trig_post_figure_5_multiplication_by_pure_negative_imaginary")
         verify(str(hash(png)), self.reporter)
 
-    def test_figure_6(self):
+    def test_trig_post_figure_6(self):
         cg = grapher.ComplexGrapher()
         cg.illustrate_multiplication(complex(0, 2), complex(-3, 2))
-        png = cg.save_as_png("figure_6_multiplication_by_pure_imaginary_illustrated")
+        png = cg.save_as_png("trig_post_figure_6_multiplication_by_pure_imaginary_illustrated")
         verify(str(hash(png)), self.reporter)
 
-    def test_figure_7(self):
+    def test_trig_post_figure_7(self):
         cg = grapher.ComplexGrapher()
         cg.illustrate_multiplication(complex(0, -2), complex(-3, 2))
-        png = cg.save_as_png("figure_7_multiplication_by_pure_negative_imaginary_illustrated")
+        png = cg.save_as_png("trig_post_figure_7_multiplication_by_pure_negative_imaginary_illustrated")
         verify(str(hash(png)), self.reporter)
 
     def test_illustrate_multiplication(self):
